0ec16ca42d738d96d763a88335cdb8907a2c492e
TalhaWD <you@example.com>
2024-01-18 21:09:16 +0500

    first commit

---
+++ public/script.js
@@ -0,0 +1,25 @@
+
+// gsap.from(".one img, .one h1, .para p, .head h1, .head h2",{
+//     y:-100,
+//     duration:1,
+//     opacity:0,
+//     stagger:0.3,
+//   })
+// // gsap.from(".para p",{
+// // y:100,
+// // opacity:0,
+// // duration:1,
+// // delay:1.5,
+// // })
+// // gsap.from(".head h1", {
+// //     y:100,
+// //     opacity:0,
+// //     duration:1,
+// //     delay:2,
+// // })
+// gsap.from(".imagetwo  ",{
+//    x:100,
+//    duration:1,
+//    delay:2.2,
+//    opacity:0,
+// })
